0f17da8cbbb8f0cc235f6c0f45f1ef85d19f586a
psyborg <psyborg@1d59d09c-6218-0410-9fd0-dc2671efdd90>
2006-07-17 12:16:29 +0000

    copied and changed nrEngine.h into top directory to easely bind them into devcpp projects

---
+++ nrEngine/nrEngine/nrEngine.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *                                                                         *
+ *   (c) Art Tevs, MPI Informatik Saarbruecken                             *
+ *       mailto: <tevs@mpi-sb.mpg.de>                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef __NR_ENGINE_H_
+#define __NR_ENGINE_H_
+
+/*--------------------------------------------------------------------------
+ * Includes
+ *------------------------------------------------------------------------*/
+#include "include/Prerequisities.h"
+#include "include/Profiler.h"
+#include "include/Dator.h"
+#include "include/NameValuePairs.h"
+#include "include/Priority.h"
+#include "include/ISingleton.h"
+#include "include/ISingletonTask.h"
+#include "include/ITask.h"
+#include "include/Kernel.h"
+#include "include/Engine.h"
+#include "include/Exception.h"
+#include "include/Log.h"
+#include "include/Clock.h"
+#include "include/ResourceSystem.h"
+#include "include/PluginLoader.h"
+#include "include/Plugin.h"
+#include "include/IStream.h"
+
+#include "include/ScriptEngine.h"
+#include "include/EventManager.h"
+#include "include/Binding.h"
+
+#endif
